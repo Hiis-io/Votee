ecafe57ae1d5590a5c3e1f5dcf948d21030c614d
icemc <abandaludovic500@gmail.com>
2022-03-30 14:09:13 +0100

    Added Super Majority rule

---
+++ src/main/scala/votee/algorithms/SuperMajority.scala
@@ -0,0 +1,27 @@
+package votee.algorithms
+
+import votee.models.{Candidate, Election, PreferenceBallot, PreferentialCandidate, Winner}
+import votee.utils.Rational
+
+import scala.collection.mutable
+
+/**
+ * Created by Abanda Ludovic on 30/03/2022.
+ */
+
+trait MajorityWithPercentage[C <: Candidate, B <: PreferenceBallot[C]](majorityPercentage: Rational = Rational(1,2)) extends Election[C, B, Winner[C]]:
+  require(!(majorityPercentage < Rational(1,2)))
+  override def run(ballots: List[B], candidates: List[C], vacancies: Int): List[Winner[C]] =
+    val candidateScoreMap = new mutable.HashMap[C, Rational]
+
+    //We are interested only in the first Candidate in the ballot and by default a score of 1/1 is granted to the candidate
+    for (ballot <- ballots)
+      candidateScoreMap(ballot.preferences.head) =
+        Rational(1) * ballot.weight + candidateScoreMap.getOrElse(ballot.preferences.head, Rational(0))
+
+    candidateScoreMap.toList.sortWith(_._2 > _._2).map(Winner(_)).filter(w => w.score > majorityPercentage).take(vacancies)
+  end run
+end MajorityWithPercentage
+
+final case class SuperMajority(majorityPercentage: Rational) extends MajorityWithPercentage[PreferentialCandidate, PreferenceBallot[PreferentialCandidate]](majorityPercentage)
+
