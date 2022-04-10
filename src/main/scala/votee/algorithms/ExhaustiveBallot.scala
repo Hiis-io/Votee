bc0fc47fb7f52e39b817256f38ceff47ac61f772
icemc <abandaludovic500@gmail.com>
2022-04-10 12:59:37 +0100

    Made EchausiveBallot Rule more generic

--- src/main/scala/votee/algorithms/ExhaustiveBallot.scala
+++ src/main/scala/votee/algorithms/ExhaustiveBallot.scala
@@ -2,13 +2,16 @@ package votee.algorithms
 
 import votee.models.{Ballot, Candidate, PreferentialBallot, PreferentialCandidate, PreferentialElection, Winner}
 
+import scala.annotation.tailrec
+
 /**
  * Created by Abanda Ludovic on 01/04/2022.
  * Algorithm: https://en.wikipedia.org/wiki/Exhaustive_ballot
  */
 
-trait ExhaustiveBallot[C <: Candidate, B <: Ballot[C]] extends PreferentialElection[C, B]:
-  override def run[CC <: C, BB <: B](ballots: List[BB], candidates: List[CC], vacancies: Int): List[Winner[C]] = {
+trait ExhaustiveBallotRule[C <: Candidate, B <: Ballot[C]] extends PreferentialElection[C, B]:
+  @tailrec
+  override final def run[CC <: C, BB <: B](ballots: List[BB], candidates: List[CC], vacancies: Int): List[Winner[C]] = {
     val candidateScoreMap = countFirstVotes(ballots, candidates)
     val sortedCandidateList = candidateScoreMap.toList.sortWith(_._2 < _._2)
     if (candidateScoreMap.size > 2) {
@@ -20,4 +23,4 @@ trait ExhaustiveBallot[C <: Candidate, B <: Ballot[C]] extends PreferentialElect
     }
   }
 
-case object ExhaustiveBallot extends ExhaustiveBallot[PreferentialCandidate, PreferentialBallot[PreferentialCandidate]]
+final class ExhaustiveBallot[C <: Candidate] extends ExhaustiveBallotRule[C, Ballot[C]]
