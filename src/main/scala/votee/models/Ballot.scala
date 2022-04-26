32d164f8df2a84e80458371e602114da7caf5eee
icemc <abandaludovic500@gmail.com>
2022-04-26 20:14:52 +0100

    Added Implicit Reads and Writes for Json Serialization

--- src/main/scala/votee/models/Ballot.scala
+++ src/main/scala/votee/models/Ballot.scala
@@ -1,6 +1,10 @@
 package votee.models
 
-import votee.utils.Rational
+import jdk.nashorn.api.scripting.JSObject
+import play.api.libs.json.{JsError, JsObject, JsResult, JsSuccess, JsValue, Json, OFormat, OWrites, Reads}
+import spire.math.Rational
+
+import scala.util.{Failure, Success, Try}
 
 /**
  * Created by Abanda Ludovic on 29/03/2022.
@@ -20,3 +24,27 @@ final case class PreferentialBallot[C <: Candidate](override val id: Int, overri
   override def excludeCandidates(candidates: List[C]): PreferentialBallot[C] = PreferentialBallot(id, weight, preferences.filterNot(candidates.contains(_)))
   override def includeCandidates(candidates: List[C]): PreferentialBallot[C] = PreferentialBallot(id, weight, preferences ++ candidates)
 end PreferentialBallot
+
+object PreferentialBallot {
+  given PreferentialBallotReads[A <: Candidate](using OFormat[A]): Reads[PreferentialBallot[A]] = {
+    case ballot: JsObject =>
+      Try {
+        val id = (ballot \ "id").as[Int]
+        val weight = (ballot \ "weight").as[Double]
+        val preferences = (ballot \ "preferences").as[List[A]]
+
+        JsSuccess(new PreferentialBallot[A](id, Rational(weight), preferences))
+      } match {
+        case Success(value) => value
+        case Failure(exception) => JsError(exception.getMessage)
+      }
+    case e => JsError("expected.jsObject")
+  }
+
+  given PreferentialBallotWrites[A <: Candidate](using OFormat[A]): OWrites[PreferentialBallot[A]] = (o: PreferentialBallot[A]) => Json.obj(
+    "id" -> o.id,
+    "weight" -> o.weight.toDouble,
+    "preferences" -> o.preferences
+  )
+
+  }
