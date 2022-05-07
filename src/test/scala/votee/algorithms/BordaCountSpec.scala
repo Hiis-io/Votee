920332a5ca7cdbb9ddfc2fe37d6fc23bced54d11
icemc <abandaludovic500@gmail.com>
2022-05-07 17:31:43 +0100

    Added Borda Count Algorithm test Fixes #10

---
+++ src/test/scala/votee/algorithms/BordaCountSpec.scala
@@ -0,0 +1,25 @@
+package votee.algorithms
+
+import org.scalatest.flatspec.AnyFlatSpec
+import votee.models.{PreferentialBallot, PreferentialCandidate}
+import votee.utils.Parser
+
+/**
+ * Created by Abanda Ludovic on 07/05/2022.
+ */
+
+class BordaCountSpec extends AnyFlatSpec {
+
+  val expectedWinners = List(PreferentialCandidate("d", "D"))
+
+  "Borda Count Algorithm Test" should
+    "verify result" in
+    assert(algorithmVerification("01-candidates.json", "03-ballots.json") === expectedWinners)
+
+
+  def algorithmVerification(candidatesFile: String, ballotFile: String): List[PreferentialCandidate] =
+    val candidates: List[PreferentialCandidate] = Parser.parseCandidates("src/main/resources/" + candidatesFile)
+    val ballots: List[PreferentialBallot[PreferentialCandidate]] = Parser.parseBallot("src/main/resources/" + ballotFile)
+
+    Veto.run(ballots, candidates, 1).map(_.candidate)
+}
